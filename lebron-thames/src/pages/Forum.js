5b67b9ed0c25ef992636b39b68352963fd3f1518
Amirah Cazeneuve <amirahcazeneuve@outlook.com>
2023-01-17 13:53:43 +0000

    navbar

--- lebron-thames/src/pages/Forum.js
+++ lebron-thames/src/pages/Forum.js
@@ -1,9 +1,18 @@
+import './Forum.css'
 function Forum() {
     return (
-      <div className="App">
-        Forum
+      <div className="forum">
+         <body> 
+                <h1> <header className='pt' >OUR PERSONAL TRAINERS</header>
+                    <p></p>
+                </h1>
+          </body>
       </div>
     );
   }
+
+  //Feed page will be default for forum, the search function 
+  //will be at the top right and view by discussion thread will be when 
+  //one views a post and it expands and can view by the comments
   
   export default Forum;
--- lebron-thames/src/pages/Layout.js
+++ lebron-thames/src/pages/Layout.js
@@ -6,13 +6,13 @@ const Layout = () => {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">Forum</Link>
           </li>
           <li>
-            <Link to="/blogs">Blogs</Link>
+            <Link to="/signup">Signup</Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/login">Login</Link>
           </li>
         </ul>
       </nav>
@@ -22,4 +22,51 @@ const Layout = () => {
   )
 };
 
+// import React from 'react';
+// import {
+//   Nav,
+//   NavLink,
+//   Bars,
+//   NavMenu,
+//   NavBtn,
+//   NavBtnLink,
+// } from './NavbarElements';
+  
+// const Navbar = () => {
+//   return (
+//     <>
+//       <Nav>
+//         <Bars />
+  
+//         <NavMenu>
+//           <NavLink to='/about' activeStyle>
+//             About
+//           </NavLink>
+//           <NavLink to='/events' activeStyle>
+//             Events
+//           </NavLink>
+//           <NavLink to='/annual' activeStyle>
+//             Annual Report
+//           </NavLink>
+//           <NavLink to='/team' activeStyle>
+//             Teams
+//           </NavLink>
+//           <NavLink to='/blogs' activeStyle>
+//             Blogs
+//           </NavLink>
+//           <NavLink to='/sign-up' activeStyle>
+//             Sign Up
+//           </NavLink>
+//           {/* Second Nav */}
+//           {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+//         </NavMenu>
+//         <NavBtn>
+//           <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+//         </NavBtn>
+//       </Nav>
+//     </>
+//   );
+// };
+  
+// export default Navbar;
 export default Layout;
