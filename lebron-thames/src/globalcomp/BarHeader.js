ae3277ea18307cd6434bb23d4644331513181847
rahnuka <amirahcazeneuve@outlook.com>
2023-01-24 11:57:10 +0000

    yeah backend

--- lebron-thames/src/globalcomp/BarHeader.js
+++ lebron-thames/src/globalcomp/BarHeader.js
@@ -4,16 +4,16 @@ import Twitter from'./twitter.png'
 import Facebook from'./facebook.png'
 const Navbar = () => {
   return (
-  <div class="topnav">
-  <a class="active" href="https://www.redmarmosetstudios.com/">Home</a>
+  <div className="topnav">
+  <a className="active" href="https://www.redmarmosetstudios.com/">Home</a>
   <a href="https://www.redmarmosetstudios.com/games">Games</a>
   <a href="https://www.redmarmosetstudios.com/team">Team</a>
   <a href="https://www.redmarmosetstudios.com/careers">Careers</a>
   <a href="https://www.redmarmosetstudios.com/contact">Contact</a>
   <a href="/login">Forum</a>
-  <a class="navbar-brand" href="/">
-      <div class="lo-image">
-            <img src={ Logo } class="logo"/>
+  <a className="navbar-brand" href="/">
+      <div className="lo-image">
+            <img src={ Logo } className="logo"/>
       </div>
 </a>
 <a href="https://twitter.com/RedMarmoset" className= "twittericon"> <img src={ Twitter }/></a>
--- lebron-thames/src/pages/Signup.js
+++ lebron-thames/src/pages/Signup.js
@@ -6,59 +6,78 @@ export default class Signup extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      uname: "",
-      email: "",
-      password: "",
-    }
+      uname: "", //rah
+      email: "", //rahnuka@outlook.com
+      password: "", //rahnuka607
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    
   }
-};
-handleSubmit(); {
+handleSubmit(e) {
+  e.preventDefault();
   const { uname, email, password } = this.state;
   console.log(uname, email, password);
+  fetch("http:localhost:5000/register",{
+    method: "POST",
+    crossDomain:true,
+    headers:{
+      "Content-Type":"application/json",
+      Accept:"application/json",
+      "Access-Control-Allow-Origin":"*",
+    },
+    body:JSON.stringify({
+      uname,
+      email,
+      password
+    }),
+  }).then((res)=>res.json())
+  .then((data)=>{
+    console.log(data,"userRegister");
+  })
 }
 
-  render(); {
+  render() {
   const [passwordShown, setPasswordShown] = useState(false);
   const togglePassword = () => {
-  setPasswordShown(!passwordShown);
-  return (
-    <form className="signup" onSubmit={this.handleSubmit}>
-       <body> 
-       <header>Sign up</header>
-          <div>
-            <input type= "text" 
-            placeholder="Username" 
-            className="username"
-            onChange={(e) => this.setState({ uname: e.target.value })}
-            />
-            <input type= "email" 
-            placeholder="Email" 
-            className="email"
-            onChange={(e) => this.setState({ email: e.target.value })}
-            />
-            <input type={passwordShown ? "text" : "password"} 
-            placeholder="Password" 
-            className="password" 
-            />
-            <input type={passwordShown ? "text" : "password"} 
-            placeholder="Password Confirm" 
-            className="passwordconfirm" 
-            onChange={(e) => this.setState({ password: e.target.value })}
-            />
-            <header className='showpassword'><input type="checkbox" onClick={togglePassword}/>  Show Password</header>
+    setPasswordShown(!passwordShown); 
+    return (
+      <form className="signup" onSubmit={this.handleSubmit}>
+        <div className="noboddy"> 
+        <header>Sign up</header>
+            <div>
+              <input type= "text" 
+              placeholder="Username" 
+              className="username"
+              onChange={(e) => this.setState({ uname: e.target.value })}
+              />
+              <input type= "email" 
+              placeholder="Email" 
+              className="email"
+              onChange={(e) => this.setState({ email: e.target.value })}
+              />
+              <input type= {passwordShown ? "text" : "password"} 
+              placeholder="Password" 
+              className="password" 
+              />
+              <input type= {passwordShown ? "text" : "password"} 
+              placeholder="Password Confirm" 
+              className="passwordconfirm" 
+              onChange={(e) => this.setState({ password: e.target.value })}
+              />
+              <header className='showpassword'><input type="checkbox" onClick={togglePassword}/>  Show Password</header>
+            </div>
+          <button type="submit" className="loginbutton">
+            Sign up
+          </button>
+          <footer>
+            Already a member?
+            <a 
+              href="/login"
+              >Log in</a>
+          </footer>
           </div>
-        <button type="submit" className="loginbutton">
-          Sign up
-        </button>
-        <footer>
-          Already a member?
-          <a 
-            href="/login"
-            >Log in</a>
-        </footer>
-        </body>
-    </form>
+      </form>
   );
 }
 }
-    
+}
